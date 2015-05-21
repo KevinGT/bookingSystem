d9e99e220cddc5277b675d73d46cec35b51acad9
Kevin Talbot <kevintalbot17@gmail.com>
2015-05-21 11:32:14 +0100

    adding file as welcome screen to login or register

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/frontpage.css">
+<link rel="icon" href="images/kgtdevlogoresized.jpg" type="image/x-icon">
+<script>
+
+</script>
+</head>
+
+<body>
+<?php include_once("page_header.php"); ?>
+
+<div id="main">
+	<h3>Welcome to the appointment booking system. Please click <a href="login_page.php">here</a> to login and make an appointment.</h3>
+
+	<h3>Don't have an account? Click <a href="registration_page.php">here</a> to register and enjoy the convenience of online booking.</h3>
+</div>
+
+
+</body>
+</html>
