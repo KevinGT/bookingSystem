f95525d40945302a25b67e64d02fd304c6814f62
Kevin Talbot <kevintalbot17@gmail.com>
2015-06-29 10:01:22 +0100

    adding explode.php testing the php explode function to loop through a string and secondary to use a foreach loop to enter each word in an array into a database

---
+++ test/explode.php
@@ -0,0 +1,51 @@
+<?php
+
+$sentence = "pizza1 pizza2 pizza3 pizza4";
+
+$explode = explode(" ", $sentence);
+
+echo $explode[0];
+echo $explode[1];
+echo $explode[2];
+echo $explode[3];
+
+
+// What is the best way to loop through string and insert into mysql column?
+
+<?php
+$item_value = 'itemOne,itemTwo,itemThree';
+$item_array = explode(",",$item_value);
+
+foreach($item_array as $key => $value){
+    // insert into the db here
+    $query = "INSERT INTO table_name set item_value = '".mysql_real_escape_string($value)."', ID = '".($key + 1)."'";
+    // however you choose to connect and insert into the database goes here :)
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
